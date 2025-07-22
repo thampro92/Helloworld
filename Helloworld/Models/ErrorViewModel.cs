ad24d4a2d3b77720135ab8e2c249cdd32ea1e898
trandangtham <tdtham.it@gmail.com>
2025-07-22 21:08:40 +0700

    Add project files.

---
+++ Helloworld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Helloworld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
